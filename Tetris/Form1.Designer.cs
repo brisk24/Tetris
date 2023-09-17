b2893a020c91dfc84624e662bf8c9e1f2dc7641f
brisk24 <krivick.aleksej@yandex.ru>
2023-09-17 15:29:31 +0300

    Настройка формы

--- Tetris/Form1.Designer.cs
+++ Tetris/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelBoard = new System.Windows.Forms.Panel();
+            this.labelResoult = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelHelp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelBoard
+            // 
+            this.panelBoard.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelBoard.Location = new System.Drawing.Point(0, 1);
+            this.panelBoard.Name = "panelBoard";
+            this.panelBoard.Size = new System.Drawing.Size(250, 500);
+            this.panelBoard.TabIndex = 0;
+            // 
+            // labelResoult
+            // 
+            this.labelResoult.Location = new System.Drawing.Point(256, 9);
+            this.labelResoult.Name = "labelResoult";
+            this.labelResoult.Size = new System.Drawing.Size(145, 113);
+            this.labelResoult.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(256, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 150);
+            this.panel1.TabIndex = 2;
+            // 
+            // labelHelp
+            // 
+            this.labelHelp.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHelp.Location = new System.Drawing.Point(256, 395);
+            this.labelHelp.Name = "labelHelp";
+            this.labelHelp.Size = new System.Drawing.Size(145, 98);
+            this.labelHelp.TabIndex = 3;
+            this.labelHelp.Text = "Управление:\r\nДвижение стрелками\r\nВлево, Вправо, Вниз\r\n\r\nПеревернуть - Вверх\r\nПауз" +
+    "а - Пробел\r\nВыход - Escape";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(413, 502);
+            this.Controls.Add(this.labelHelp);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelResoult);
+            this.Controls.Add(this.panelBoard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Тетрис";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelBoard;
+        private System.Windows.Forms.Label labelResoult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelHelp;
     }
 }
 
