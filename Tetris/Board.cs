112845570b9550e8fef4701ed0cbe276417475c1
brisk24 <krivick.aleksej@yandex.ru>
2023-09-18 19:24:52 +0300

    Тестовая фигура и обновление игрового поля

--- Tetris/Board.cs
+++ Tetris/Board.cs
@@ -9,16 +9,21 @@ namespace Tetris
         int sizeY = 20;
 
         PictureBox[,] box;
+        int[,] map, mapBack;
         Panel panel;
         public Board(Panel panel)
         {
             this.panel = panel;
             InitMap();
+            AddFigureOnBoard();
+            RefreshBoard();
         }
 
         void InitMap()
         {
             box = new PictureBox[sizeX, sizeY];
+            map = new int[sizeX, sizeY];
+            mapBack = new int[sizeX, sizeY];
             int boxSize = panel.Width / sizeX;
 
             for (int x = 0; x < sizeX; x++)
@@ -32,7 +37,23 @@ namespace Tetris
 
                     panel.Controls.Add(picture);
                     box[x, y] = picture;
+                    map[x, y] = mapBack[x, y] = 0;
                 }       
         }
+
+        void AddFigureOnBoard()
+        {
+            mapBack[2, 3] = mapBack[2, 4] = mapBack[2, 5] = 1;
+        }
+
+        void RefreshBoard()
+        {
+            for (int x = 0; x < sizeX; x++)
+                for (int y = 0; y < sizeY; y++)
+                {
+                    box[x, y].BackColor = mapBack[x, y] == 0 ? SystemColors.ControlLightLight : Color.Black;
+                }
+        }
+
     }
 }
